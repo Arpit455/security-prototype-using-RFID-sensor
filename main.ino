479df0c1e84f09c58e478e47d518e1fec643667f
Arpit455 <arpitgupta455@gmail.com>
2021-07-11 13:39:51 +0530

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,65 @@
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  
+  pinMode(2,OUTPUT);
+  pinMode(7,OUTPUT);
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Place your card near to the reader...");
+  Serial.println();
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "39 B1 49 43") //change here the UID of the card/cards that you want to give access
+  {
+    
+    digitalWrite(7,HIGH);
+    Serial.println("Authorized access");
+    Serial.println("Message : GATE OPENED");
+    delay(2000);
+    digitalWrite(7,LOW);
+    Serial.println("Message : GATE CLOSED");
+    
+  }
+ 
+ else   {
+    digitalWrite(2,HIGH);
+    Serial.println("Access denied");
+    Serial.println("Message : SORRY WRONG CARD ");
+    delay(1000);
+    digitalWrite(2,LOW);
+  }
+}
